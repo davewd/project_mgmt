1575f78a4b07e38212000faf4499c5be1e9658b6
davewd <davewd@me.com>
2021-04-26 23:20:57 +1000

    feat: :zap: refactor and add more automation

--- project_mgmt/project_mgmt.py
+++ project_mgmt/project_mgmt.py
@@ -15,35 +15,58 @@ import datetime
 
 GITHUB_URL = "https://api.github.com/"
 GITHUB_OWNER = "davewd"
-GITHUB_REPO = "davewd"
+GITHUB_REPO = "project_mgmt"
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-def get_credentials(github_owner:str)->tuple:
+
+
+def get_credentials(github_owner: str) -> tuple:
     logger.info("getting credentials...")
-    f = open(f"../secrets/{github_owner}.json", "r")
+    try:
+        f = open(f"../secrets/{github_owner}.json", "r")
+    except Exception as e:
+        logger.info(
+            f"Are your owner credentials setup in the secrets folder as owner.json ? {e}")
+
     file_data = f.read()
     data = json.loads(file_data)
     logger.info(data)
     return (data["username"], data["key"])
 
-def create_jira_epic()->bool:
+
+def create_jira_epic() -> bool:
     logger.info("Creating Jira Epic")
     pass
 
-def create_jira_story()->bool:
+
+def create_jira_story() -> bool:
     pass
 
-def create_github_project()->bool:
-    pass
 
-def create_github_milestone(owner: str, repo:str, title:str, due_date: datetime.date)->bool:
-    r = requests.post(f"{GITHUB_URL}repos/{owner}/{repo}/milestones",
-                    auth=get_credentials(owner),
-                    data=json.dumps({
-                        "title": title,
-                        "due_on": due_date.isoformat()
-                    }))
+def create_github_project(owner: str, repo: str, title: str,) -> bool:
+    r = requests.post(f"{GITHUB_URL}/repos/{owner}/{repo}/projects",
+                      auth=get_credentials(owner),
+                      data=json.dumps({
+                          "owner": owner,
+                          "repo": repo,
+                          "name": f"[short]{title}",
+                          "mediaType": {
+                              "previews": [
+                                'inertia'
+                              ]
+                          }
+                      }))
     return r.status_code == http.client.OK
 
-print(get_credentials(GITHUB_OWNER))
+
+def create_github_milestone(owner: str, repo: str, title: str, due_date: datetime.date) -> bool:
+    r = requests.post(f"{GITHUB_URL}repos/{owner}/{repo}/milestones",
+                      auth=get_credentials(owner),
+                      data=json.dumps({
+                          "title": title,
+                          "due_on": due_date.isoformat()
+                      }))
+    return r.status_code == http.client.OK
+
+
+create_github_project(GITHUB_OWNER, GITHUB_REPO, "test 123")
